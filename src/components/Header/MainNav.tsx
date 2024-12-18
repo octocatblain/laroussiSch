e2a9056151e7dcd6c34dd2e9c8991c2c3f4821c0
Blain Muema <blainmuema@gmail.com>
2024-12-18 22:08:26 +0300

    correct gitignore

--- src/components/Header/MainNav.tsx
+++ src/components/Header/MainNav.tsx
@@ -1,15 +1,18 @@
-import { RiSearch2Line } from 'react-icons/ri';
+'use client';
 
+import { options } from '@/app/api/auth/[...nextauth]/options';
 import { NavLinks } from '@/data/content';
 import Logo from '@/shared/Logo/Logo';
-
-// import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { getServerSession } from 'next-auth';
+import { signOut } from 'next-auth/react';
 import Link from 'next/link';
+import { RiSearch2Line } from 'react-icons/ri';
 import CartSideBar from '../CartSideBar';
 import NavigationItem from '../NavItem';
 import MenuBar from './MenuBar';
 
 const MainNav = () => {
+  const session: any = getServerSession(options);
   return (
     <div className="container flex items-center justify-between">
       <div className="flex-1">
@@ -25,20 +28,21 @@ const MainNav = () => {
       <div className="hidden flex-1 items-center justify-end gap-7 lg:flex">
         <RiSearch2Line className="text-2xl" />
 
-        <Link href="/login">
-
-          <p className="text-2xl">Login</p>
-          {/* <LuUser2Line className="text-2xl" /> */}
-        </Link>
-        {/* <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedIn> */}
-        <CartSideBar />
-        {/* </SignedIn> */}
+        {session ? (
+          <>
+            <button
+              onClick={() => signOut()}
+              className="text-lg text-red-500 hover:underline"
+            >
+              Sign Out
+            </button>
+            <CartSideBar />
+          </>
+        ) : (
+          <Link href="/login">
+            <p className="text-lg">Login</p>
+          </Link>
+        )}
       </div>
 
       <div className="lg:hidden">
